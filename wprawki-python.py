a52ab8b1ad8610486bab176863e82ab05a056e76
hydra-create <niewychodzi@gmail.com>
2025-07-23 14:15:31 +0200

    Update wprawki-python.py
    Dodano licznik słów w tekście

--- wprawki-python.py
+++ wprawki-python.py
@@ -7,3 +7,17 @@ i = 1
 while i <= len(ciag_znakow):
     print(ciag_znakow[-i], end = "")
     i += 1
+
+# Wprawki Python - licznik słów w tekście
+
+tekst = input("Wprowadź tekst, w którym chcesz policzyć słowa: ")
+
+spacja = 0
+
+# W pętli zliczane są spacje w tekście
+for znak in tekst:
+    if znak == " ":
+        spacja += 1
+
+# Słów w tekście jest o 1 więcej niż spacji
+print("Liczba słów w tekscie: ", spacja + 1)
