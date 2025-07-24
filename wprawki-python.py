7a56c61c3b1d6080cc6947f28390492ec3057272
hydra-create <niewychodzi@gmail.com>
2025-07-24 10:07:19 +0200

    Update wprawki-python.py
    Dodano zjadacz samogłosek

--- wprawki-python.py
+++ wprawki-python.py
@@ -38,3 +38,21 @@ while secret_number != user_number:
     user_number = int(input("Zgadnij numer: "))
     
 print("Dobra robota! Jesteś wolny!")
+
+# Wprawki Python - zjadacz samogłosek
+user_word = input("Wprowadź słowo: ")
+user_word = user_word.upper()
+
+for letter in user_word:
+    if letter == "A":
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "U":
+        continue
+    else:
+        print(letter)
